242b64f3f7e98eca7fcf19c2ff2028e020e46940
Kuga09 <glal64@tutamail.com>
2024-09-30 14:55:42 +0300

    Шаблон

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-input_data = open('input.txt','r')
-data = input_data.read()
+input_data = open('input.txt','r') # Открываем для чтения файл
+data = input_data.read() # Читаем в другую переменную содержимое всего файла
 
-output_data = open('output.txt','w')
-output_data.write(c)
+output_data = open('output.txt','w')# Открываем для записи файл
+output_data.write(c) # Записываем результат сложения output_data.write(str(int(data[0])+int(data[1])))
 
 input_data.close()
 output_data.close()
